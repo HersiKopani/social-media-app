d8e2f0147c2cdb6b455775bcd642995984ce5f44
HersiKopani <hersi.kopani@gmail.com>
2021-02-14 20:24:10 +0100

    register user

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const {ApolloServer} = require ('apollo-server'); 
+const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
-const {MONGODB} = require('./config.js');
-
-    const typeDefs = gql`
-    type Query{
-            sayHi: String!
-        }
-    `;
-    
-     
-
-    const resolvers = {
-        Query:{
-            sayHi: () => 'Hello world'
-        }
-    };
+const { MONGODB } = require('./config.js');
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
     typeDefs,
@@ -23,12 +11,12 @@ const server = new ApolloServer({
 });
 
 mongoose
-    .connect(MONGODB, {useNewUrlParser: true})
+    .connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
         console.log('MongoDb Connected');
-        return server.listen({port:5000});
+        return server.listen({ port: 5000 });
     })
-    .then((res)=> {
+    .then((res) => {
         console.log(`Server runnign at ${res.url}`)
     })
 
